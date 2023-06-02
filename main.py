47f9d9a3c1dadd69001aae9e5f06bfcde7209851
SLiedtkeDev <stephanliedtke92@gmail.com>
2023-06-02 07:08:53 -0600

    Funcionalidad de Tabla casos Maximos

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-def crear_matriz():
+from matplotlib import pyplot as plt
+
+headers_tablas = ['Estado', 'Fecha', 'Maximo']
+
+
+def grafica_linea(x, y):
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.plot(x, y)
+    plt.xticks(x, rotation=90)
+    plt.show()
+
+
+def muestra_tabla(datos, headers):
+    fig, ax = plt.subplots(figsize=(20, 10))
+    ax.table(cellText=datos, loc="center", colLabels=headers)
+    ax.axis('tight')
+    ax.axis('off')
+    plt.show()
+
+
+def crear_matriz_inicial():
     matriz = []
     with open("casos_covid.csv", "r") as file:
         linea = True
@@ -32,11 +52,26 @@ def crear_menu():
     print(70 * '*')
 
 
-def crear_tabla_maximos_casos(matriz):
-    pass
+def tabla_plot_maximos_casos(matriz):
+    matriz_casos = []
+    for i in range(1, len(matriz)):
+        F = []
+        estado = matriz[i][2]
+        F.append(estado)
+        casos_maximos = 0
+        indice = None
+        for j in range(3, len(matriz[0])):
+            if int(matriz[i][j]) > casos_maximos:
+                indice = j
+                casos_maximos = int(matriz[i][j])
+        F.append(casos_maximos)
+        fecha = matriz[0][indice]
+        F.append(fecha)
+        matriz_casos.append(F)
+    muestra_tabla(matriz_casos, headers_tablas)
 
 
-def crear_tabla_casos_porcentaje(matriz):
+def tabla_plot_casos_porcentaje(matriz):
     pass
 
 
@@ -46,21 +81,21 @@ def crear_linea_tiempo(matriz):
 
 def main():
     crear_menu()
-    datos = crear_matriz()
+    datos = crear_matriz_inicial()
     while True:
         opcion = int(input("Que opcion deseas elegir: "))
         if opcion == 4:
             print("Vuelva pronto, gracias por usar nuestro sistema!")
             break
         elif opcion == 1:
-            crear_tabla_maximos_casos()
+            tabla_plot_maximos_casos(datos)
         elif opcion == 2:
-            crear_tabla_casos_porcentaje()
+            tabla_plot_casos_porcentaje()
         elif opcion == 3:
             crear_linea_tiempo()
         else:
             print("Opcion no valida, vuelva a elegir")
 
-    print("test")
+
 if __name__ == "__main__":
     main()
