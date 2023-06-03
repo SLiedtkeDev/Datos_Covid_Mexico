a5fe92f31d73e0b4fdd9d896003840379b7e1130
SLiedtkeDev <stephanliedtke92@gmail.com>
2023-06-02 22:16:45 -0600

    Correciones menores finales

--- main.py
+++ main.py
@@ -72,6 +72,7 @@ def tabla_plot_maximos_casos(matriz):
     matriz_casos = []
     eje_x = []
     eje_y = []
+
     for i in range(1, len(matriz)):
         F = []
         estado = matriz[i][2]
@@ -88,18 +89,21 @@ def tabla_plot_maximos_casos(matriz):
         F.append(casos_maximos)
         matriz_casos.append(F)
     muestra_tabla(matriz_casos, headers_tablas_maximos)
+
     for i in range(len(matriz_casos)-1):
         x = matriz_casos[i][0]
         y = matriz_casos[i][2]
         eje_x.append(x)
         eje_y.append(y)
-    grafica_linea(x=eje_x, y=eje_y, title='Máximos  # de contagios por estado')
+    grafica_linea(x=eje_x, y=eje_y,
+                  title='Máximos  # de contagios por estado')
 
 
 def tabla_plot_casos_porcentaje(matriz):
     matriz_porcentajes = []
     eje_x = []
     eje_y = []
+
     for i in range(1, len(matriz)):
         F = []
         estado = matriz[i][2]
@@ -115,6 +119,7 @@ def tabla_plot_casos_porcentaje(matriz):
         F.append(porcentaje_infeccion)
         matriz_porcentajes.append(F)
     muestra_tabla(matriz_porcentajes, headers_tablas_porcentaje)
+
     for i in range(len(matriz_porcentajes)-1):
         x = matriz_porcentajes[i][0]
         y = matriz_porcentajes[i][3]
@@ -126,59 +131,62 @@ def tabla_plot_casos_porcentaje(matriz):
 def crear_linea_tiempo(matriz):
     lugar_no_encontrado = True
     posicion_lugar = -1
-    
+
     while lugar_no_encontrado:
+        print(70 * '*')
+        print("En Lugar puedes teclear el nombre de algun estado o Nacional")
+        print(70 * '*')
         lugar = input("Lugar -> ").upper()
         posicion_lugar = buscar_lugar(matriz, lugar)
-        
+
         if posicion_lugar != -1:
             lugar_no_encontrado = False
         else:
             print("Lugar inválido")
-            
+
     cabecera, datos = obtener_cabecera_datos_lugar(matriz, posicion_lugar)
     lista_meses, lista_datos = obtener_listas_meses_datos(cabecera, datos)
     titulo = "Serie de tiempo mensual para " + lugar
-    
+
     grafica_linea(lista_meses, lista_datos, title=titulo)
-    
-        
+
+
 def obtener_listas_meses_datos(cabecera, datos):
     n_columnas = len(cabecera)
     mes_anterior = cabecera[3]
-    suma  = 0
+    suma = 0
     lista_meses = []
     lista_datos = []
-    
-    for i in range(3, n_columnas):       
+
+    for i in range(3, n_columnas):
         dato = int(datos[i])
         mes_actual = cabecera[i]
         mes_numero = mes_actual.split('-')[1]
         mes_numero_anterior = mes_anterior.split('-')[1]
-        
-        if  (mes_numero == mes_numero_anterior):
+
+        if (mes_numero == mes_numero_anterior):
             suma += dato
         else:
             lista_meses.append(mes_actual[3:])
             lista_datos.append(suma)
-            
+
             mes_anterior = mes_actual
             suma = 0
-            
+
     return lista_meses, lista_datos
-     
-     
+
+
 def obtener_cabecera_datos_lugar(matriz, posicion_lugar):
     return matriz[0], matriz[posicion_lugar]
-    
-    
+
+
 def buscar_lugar(matriz, lugar):
     for i in range(1, len(matriz)):
         nombre = matriz[i][2].upper().replace("\"", '')
-        
+
         if nombre == lugar:
             return i
-        
+
     return -1
 
 
@@ -186,15 +194,15 @@ def main():
     crear_menu()
     datos = crear_matriz_inicial()
     while True:
-        opcion = int(input("Que opcion deseas elegir: "))
-        if opcion == 4:
+        opcion = input("Que opcion deseas elegir: ")
+        if opcion == '4':
             print("Vuelva pronto, gracias por usar nuestro sistema!")
             break
-        elif opcion == 1:
+        elif opcion == '1':
             tabla_plot_maximos_casos(datos)
-        elif opcion == 2:
+        elif opcion == '2':
             tabla_plot_casos_porcentaje(datos)
-        elif opcion == 3:
+        elif opcion == '3':
             crear_linea_tiempo(datos)
         else:
             print("Opcion no valida, vuelva a elegir")
